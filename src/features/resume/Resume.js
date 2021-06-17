4796d230633b10873d34687f73c096b1ac272877
DanNeasmith <DanNeasmith@gmail.com>
2021-06-17 19:14:48 -0400

    working pdf

--- src/features/resume/Resume.js
+++ src/features/resume/Resume.js
@@ -1,12 +1,12 @@
 // import styles from './Resume.module.css';
-import { Document, Page } from 'react-pdf';
+// import React, { useState } from 'react';
 
 export function Resume() {
+
     return (
-        <div>
-            <Document file='../../../public/Daniel_Neasmith_CV.pdf' >
-                <Page />
-            </Document>
+        <div style={{ height: '78vh', textAlign: 'center' }} >
+            {/* <h1>Resume</h1> */}
+            <iframe src='./Daniel_Neasmith_CV.pdf' width='90%' height='100%'></iframe>
         </div>
-    )
+    );
 }

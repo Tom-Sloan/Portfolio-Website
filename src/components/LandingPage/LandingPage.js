96877cc681d4770d0a31ece11b8666de29e19b29
tomsloan1 <tsloan81@gmail.com>
2021-06-19 20:13:53 -0400

    got matter.js working, added some basic landing page feature

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,55 @@
+import { useRef, useEffect, useState } from "react";
+import World from "./World";
+import styles from "./LandingPage.module.css";
+
+export function LandingPage() {
+  const landingPageRef = useRef(null);
+  const [world, setWorld] = useState();
+
+  useEffect(() => {
+    if (document.cookie.includes("landingPage")) {
+      console.log("here");
+      // document
+      //   .querySelector(".App")
+      //   .scrollTo(0, landingPageRef.current.clientHeight);
+    } else {
+      console.log("not here");
+      document.cookie =
+        "landingPage=visited; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    }
+
+    //Add world
+    setWorld(World(landingPageRef.current));
+
+    document
+      .querySelector(".App")
+      .addEventListener("scroll", handleRestart);
+
+    return window.removeEventListener("scroll", handleScroll);
+  }, []);
+  
+  const handleScroll = (e) => {
+    console.log("wheel");
+    document
+      .querySelector(".App")
+      .scrollTo(0, landingPageRef.current.clientHeight);
+    setTimeout(()=>world.stop(), 3000);
+  };
+  const handleRestart = ({target}) => {
+    if(!target.scrollTop){
+      setWorld(World(landingPageRef.current));
+    }
+  };
+
+  return (
+    <div
+      id="LandingPage"
+      ref={landingPageRef}
+      style={{ height: "100vh" }}
+      onWheel={handleScroll}
+      className={styles.landingPageContainer}
+    >
+      {/* <World /> */}
+    </div>
+  );
+}
